0bfad0de480a2b5ee53e77cf4051fc438432f2e5
Bhone Pyae Kyaw <bhonepyaekyaw1899@gmail.com>
2022-03-29 09:52:59 +0630

    Added loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import "../styles/globals.css";
+import Router from "next/router";
 import Layout from "../components/Layout";
+import ProgressBar from "@badrap/bar-of-progress";
+
+const progress = new ProgressBar({
+  size: 2,
+  color: "#3B82F6",
+  className: "bar-of-progress",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }) {
   return (
