9e903d442f0502b5aa0e37d21fb01325f5822375
Bhone Pyae Kyaw <bhonepyaekyaw1899@gmail.com>
2022-04-04 16:09:26 +0630

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import Router from "next/router";
+import Script from "next/script";
 import Layout from "../components/Layout";
 import ProgressBar from "@badrap/bar-of-progress";
 
@@ -16,9 +17,26 @@ Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
