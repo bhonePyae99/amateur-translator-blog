76948dfd4d6d1cda1c7ce9a0972a7c2346d3cc43
Bhone Pyae Kyaw <bhonepyaekyaw1899@gmail.com>
2022-04-06 15:23:57 +0630

    useable version 2

--- pages/_app.js
+++ pages/_app.js
@@ -19,21 +19,21 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script
-        id="ga-1"
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
-      <Script strategy="lazyOnload" id="ga-2">
+      <Script id="google-analytics" strategy="lazyOnload">
         {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-        page_path: window.location.pathname,
-        });
-    `}
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
       </Script>
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
